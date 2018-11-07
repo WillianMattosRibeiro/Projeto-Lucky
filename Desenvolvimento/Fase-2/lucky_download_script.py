26fc2b61ac4e8d1d073d112f07e1c8a594a8e648
DESKTOP-033V8FT\BlueShift 017 <willian.ribeiro@blueshift.com.br>
2018-11-07 11:11:25 -0200

    Teste tratamento de erros

--- Desenvolvimento/Fase-2/lucky_download_script.py
+++ Desenvolvimento/Fase-2/lucky_download_script.py
@@ -1,20 +1,42 @@
 #Download Script
 
 #Bibliotecas
-import datetime, os, requests
+import datetime, os, requests, zipfile, StringIO
 
-#Recebe o argumento "url" que possui o link de download e o "path" que recebe o caminho onde deseja extrair os dados do arquivo baixado
-def downloadAndExtract(url, path):
+# Argumentos #
+link_megasena = 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip'
+file_path = '/download/MegaSena/'
+
+def downloadFile(url):
     r = requests.get(url, stream='true')
+    return r
 
-def putDateInPath(path):
-    return path + datetime.date.today().strftime("%Y-%m-%d") + "/"
+def putFileInPath(f, path):
+	# Concatenando diretorio completo com data
+    fullPath = os.getcwd() + path + getDate()
+    
+    if not os.path.exists(fullPath):
+		os.makedirs(fullPath)
 
-def getActualDate():
+    open(fullPath + "/megasena.zip", 'wb').write(f.content)
+
+def getDate():
     return datetime.date.today().strftime("%Y-%m-%d")
 
 print ("# Baixando Arquivos... \n\n")
-#Download Resultado MegaSena
-downloadAndExtract('http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip', putDateInPath(os.getcwd() + '/download/raw/MegaSena/'))
+try:
+	f = downloadFile(link_megasena)
+except Exception, e:
+	e = sys.exc_info()[0]
+else:
+	print ("# Falha ao efetuar o download do arquivo... \n\n")
+
+print ("# Salvando Arquivos No diretorio... \n\n")
+try:
+	putFileInPath(f, file_path)
+except Exception, e:
+	e = sys.exc_info()[0]
+else:
+	print ("# Falha ao salvar o arquivo \n\n")
 
 print ("# Script Finalizado com sucesso:!!!")
