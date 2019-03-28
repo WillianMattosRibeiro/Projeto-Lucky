3078675aca40fe428074692d44954e5ba244968a
Willian Mattos Ribeiro <will07_mattos@hotmail.com>
2019-03-28 16:49:53 -0300

    Reestruturação de diretorios
    Reestruturando diretórios e organizando arquivos

--- projeto_lucky/source/run_process.py
+++ projeto_lucky/source/run_process.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 #import v1 as inicio_1
-import v2 as inicio_2
+import projeto_lucky as inicio_2
 
 #if __name__ == '__main__':
 #    sys.exit(inicio_1.main(sys.argv))
